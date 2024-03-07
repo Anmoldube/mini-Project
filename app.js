0f14e9e019967ebfb70c0821b2150ccff050ec20
Anmol Dube <114559810+Anmoldube@users.noreply.github.com>
2024-03-07 22:01:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+let input =  document.querySelector('input');
+let h2 =  document.querySelector('h2');
+input.addEventListener('input', function(e){
+h2.innerText = e.target.value;
+
+})
+// h.addEventListener('input',fun);
+let themChange = document.querySelector('body')
+themChange.setAttribute('class' , 'anol');
+
+let then = document.querySelector('onclick')
+then.setAttribute('class' , 'an');
+
+
+function func() {
+let then = document.querySelector('body')
+then.setAttribute('class' , 'anmil');
+}
+
+
+function thish() {
+let then = document.querySelector('body')
+then.setAttribute('class' , 'an');
+    }
+
+let button1 = document.querySelector('button');
+button1.onmouseenter= function(){
+button1.style.color = 'red';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
